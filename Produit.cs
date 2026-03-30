7eb8031ac7c2042211ce547206742d566a488055
ant1Da <antoine.dast@edouardgand.fr>
2026-03-30 16:47:22 +0200

    Milieu exercice 3

---
+++ Produit.cs
@@ -0,0 +1,36 @@
+class Produit
+{
+    private string codeArticle;
+    private string designation;
+    private double prix;
+    public Produit(string code, string nom, double px)
+    {
+        this.codeArticle = code;
+        this.designation = nom;
+        this.prix = px;
+    }
+    public string GetCodeArticle()
+    {
+        return codeArticle;
+    }
+    public string GetDesignation()
+    {
+        return designation;
+    }
+    public string GetPrix()
+    {
+        return prix.ToString();
+    }
+    public override string ToString()
+    {
+        return $"Code: {this.codeArticle}, Designation: {this.designation}, Prix: {this.prix}";
+    }
+    public override bool Equals(object obj)
+    {
+        if (obj is Produit autre)
+        {
+            return this.codeArticle == autre.codeArticle;
+        }
+        return false;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Produit p1 = new Produit("1754863254","LEGO Batman 4",69.99);
+        Console.WriteLine(p1.ToString());
+        Console.WriteLine(p1);
+
+        // Exercice 2
+        Produit p2 = new Produit("AO", "Souris", 15);
+        Produit p3 = new Produit("AO", "Souris", 15);
+        Console.WriteLine(p2.Equals(p3));
+        HashSet<Produit> panier = new HashSet<Produit>();
+        panier.Add(p3);
+        panier.Add(p2);
+        Console.WriteLine(panier.Count);
     }
 }
